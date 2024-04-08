c7b1016f6f3feba1ed0d4a94065b87914fc163e8
Oscar Byiringiro <byiringoroscar@gmail.com>
2024-04-08 23:01:09 +0200

    Add table

--- core/urls.py
+++ core/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('content/', views.htmx_content, name='htmx-content'),
 ]
--- core/views.py
+++ core/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def htmx_content(request):
+    return render(request, 'partials/user-table.html')
